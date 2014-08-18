f52f4331ecf306cb35c21e529266d014e9c26340
Reika <reikasminecraft@gmail.com>
2014-08-17 23:10:50 -0400

    Update

--- World/CustomEndProvider.java
+++ World/CustomEndProvider.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * @author Reika Kalseki
+ * 
+ * Copyright 2014
+ * 
+ * All rights reserved.
+ * Distribution of the software in any form is only allowed with
+ * explicit, prior permission from the owner.
+ ******************************************************************************/
 package Reika.ChromatiCraft.World;
 
 import net.minecraft.util.MathHelper;
