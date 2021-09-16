a483a62d07dde0fc2cc84d4f2bfc2f06ff657d67
Reika <reikasminecraft@gmail.com>
2021-09-15 22:42:25 -0400

    Update 1072

--- API/ChromatiAPI.java
+++ API/ChromatiAPI.java
@@ -3,48 +3,27 @@ package Reika.ChromatiCraft.API;
 
 public abstract class ChromatiAPI {
 
-	protected AbilityAPI abilities;
-	protected CastingAPI recipes;
-	protected RitualAPI rituals;
-
-	protected ProgressionAPI research;
-	protected PlayerBufferAPI buffers;
-
-	protected AuraLocusAPI aura;
-	protected AdjacencyUpgradeAPI adjacency;
-
-	protected ItemElementAPI items;
-	protected CrystalPotionAPI potions;
-
-	protected RuneAPI runes;
-	protected DyeTreeAPI trees;
-
 	private static ChromatiAPI core;
 
 	public static ChromatiAPI getAPI() {
-		if (!core.initialized())
-			core.initalize();
 		return core;
 	}
 
-	public final AbilityAPI abilities() {return abilities;}
-	public final CastingAPI recipes() {return recipes;}
-	public final RitualAPI rituals() {return rituals;}
+	public abstract AbilityAPI abilities();
+	public abstract CastingAPI recipes();
+	public abstract RitualAPI rituals();
 
-	public final ProgressionAPI research() {return research;}
-	public final PlayerBufferAPI buffers() {return buffers;}
+	public abstract ProgressionAPI research();
+	public abstract PlayerBufferAPI buffers();
 
-	public final AuraLocusAPI aura() {return aura;}
-	public final AdjacencyUpgradeAPI adjacency() {return adjacency;}
+	public abstract AuraLocusAPI aura();
+	public abstract AdjacencyUpgradeAPI adjacency();
 
-	public final ItemElementAPI items() {return items;}
-	public final CrystalPotionAPI potions() {return potions;}
+	public abstract ItemElementAPI items();
+	public abstract CrystalPotionAPI potions();
 
-	public final RuneAPI runes() {return runes;}
-	public final DyeTreeAPI trees() {return trees;}
-
-	protected abstract void initalize();
-	protected abstract boolean initialized();
+	public abstract RuneAPI runes();
+	public abstract DyeTreeAPI trees();
 
 
 }
--- Auxiliary/APIImpl/CCAPICore.java
+++ Auxiliary/APIImpl/CCAPICore.java
@@ -3,8 +3,17 @@ package Reika.ChromatiCraft.Auxiliary.APIImpl;
 import java.lang.reflect.Field;
 
 import Reika.ChromatiCraft.ChromatiCraft;
+import Reika.ChromatiCraft.API.AbilityAPI;
+import Reika.ChromatiCraft.API.AdjacencyUpgradeAPI;
+import Reika.ChromatiCraft.API.AuraLocusAPI;
+import Reika.ChromatiCraft.API.CastingAPI;
 import Reika.ChromatiCraft.API.ChromatiAPI;
+import Reika.ChromatiCraft.API.CrystalPotionAPI;
+import Reika.ChromatiCraft.API.DyeTreeAPI;
+import Reika.ChromatiCraft.API.ItemElementAPI;
+import Reika.ChromatiCraft.API.PlayerBufferAPI;
 import Reika.ChromatiCraft.API.ProgressionAPI;
+import Reika.ChromatiCraft.API.RitualAPI;
 import Reika.ChromatiCraft.API.RuneAPI;
 import Reika.ChromatiCraft.Auxiliary.Ability.AbilityHelper;
 import Reika.ChromatiCraft.Auxiliary.RecipeManagers.AbilityRituals;
@@ -16,44 +25,9 @@ import Reika.DragonAPI.Exception.RegistrationException;
 
 public class CCAPICore extends ChromatiAPI {
 
-	private boolean init;
-
-	@Override
-	protected void initalize() {
-		try {
-			abilities = AbilityHelper.instance;
-			recipes = RecipesCastingTable.instance;
-			rituals = AbilityRituals.instance;
-
-			research = ProgressionAPI.instance;
-			buffers = PlayerElementBuffer.instance;
-
-			aura = new AuraLocusAPIImpl();
-			adjacency = new AdjacencyUpgradeAPIImpl();
-
-			items = ItemElementCalculator.instance;
-			potions = CrystalPotionController.instance;
-
-			runes = RuneAPI.instance;
-			trees = new DyeTreeAPIImpl();
-		}
-		catch (Throwable e) {
-			if (e instanceof LinkageError) {
-				ChromatiCraft.logger.logError("Could not initialize API: "+e.toString());
-				e.printStackTrace();
-			}
-			else {
-				throw new RegistrationException(ChromatiCraft.instance, "Could not initialize API core", e);
-			}
-		}
-
-		init = true;
-	}
-
-	@Override
-	protected boolean initialized() {
-		return init;
-	}
+	private final AuraLocusAPIImpl aura = new AuraLocusAPIImpl();
+	private final AdjacencyUpgradeAPIImpl adjacency = new AdjacencyUpgradeAPIImpl();
+	private final DyeTreeAPIImpl trees = new DyeTreeAPIImpl();
 
 	public static void load() {
 		try {
@@ -64,6 +38,61 @@ public class CCAPICore extends ChromatiAPI {
 		catch (Exception e) {
 			throw new RegistrationException(ChromatiCraft.instance, "Could not construct API core", e);
 		}
+	}
+
+	@Override
+	public AbilityAPI abilities() {
+		return AbilityHelper.instance;
+	}
+
+	@Override
+	public CastingAPI recipes() {
+		return RecipesCastingTable.instance;
+	}
+
+	@Override
+	public RitualAPI rituals() {
+		return AbilityRituals.instance;
+	}
+
+	@Override
+	public ProgressionAPI research() {
+		return ProgressionAPI.instance;
+	}
+
+	@Override
+	public PlayerBufferAPI buffers() {
+		return PlayerElementBuffer.instance;
+	}
+
+	@Override
+	public AuraLocusAPI aura() {
+		return aura;
+	}
+
+	@Override
+	public AdjacencyUpgradeAPI adjacency() {
+		return adjacency;
+	}
+
+	@Override
+	public ItemElementAPI items() {
+		return ItemElementCalculator.instance;
+	}
+
+	@Override
+	public CrystalPotionAPI potions() {
+		return CrystalPotionController.instance;
+	}
+
+	@Override
+	public RuneAPI runes() {
+		return RuneAPI.instance;
+	}
+
+	@Override
+	public DyeTreeAPI trees() {
+		return trees;
 	}
 
 }
